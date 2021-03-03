68a21558dfa3b1b0651b0a8dac84207cda71f997
kmundil <kelliejmundil@gmail.com>
2021-03-03 22:56:11 +0000

    fixed spacing

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -15,9 +15,8 @@ const oldPointStructure = {
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
- 
 	for (let i = 0; i < word.length; i++) {
- 
+
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
@@ -32,26 +31,74 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+function initialPrompt(word) {
+ 
+   console.log("Let's play some scrabble! \n ");
+    return input.question("Enter a word to score: ");
+
 };
 
-let simpleScore;
+let simpleScore = function(word) {
+  word = word.toUpperCase();
+  let simplePoints = 0;
+  for (i=0; i < word.length; i++) {
+    simplePoints++;
+    }
+    return simplePoints;
+    
+  }
 
-let vowelBonusScore;
+let vowelBonusScore = function(word) {
+  word = word.toUpperCase();
+  let bonusPoints = 0;
+  for (i=0; i < word.length; i++) {
+    if (word[i] === 'A' || word[i] === 'E' || word[i] === 'I' || word[i] === 'O' || word[i] === 'U') {
+      bonusPoints += 2;
+    }
+    bonusPoints += 1;
+
+  }
+  return bonusPoints;
+};
 
 let scrabbleScore;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  { name: 'Simple Score', description: 'Each letter is worth 1 point', scorerFunction: simpleScore },
+  { name: 'Bonus Vowels', description: 'Vowels are 3 pts, consonants are 1 pt.', scorerFunction: vowelBonusScore }, 
+  { name: 'Scrabble', description: 'The traditional scoring algorithm', scorerFunction: oldScrabbleScorer}
 
-function scorerPrompt() {}
+];
 
-function transform() {};
+function scorerPrompt(word) {
+  let number = input.question(`which scoring algorithm would you like to use?\n
+0 - ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].description}
+1 - ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].description}
+2 - ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].description}
+  Enter 0, 1, or 2: `)
 
-let newPointStructure;
+  if (number === '0'){
+    console.log("algorithm name: ", scoringAlgorithms[0].name);
+    return `scoreFunction result: ${scoringAlgorithms[0].scorerFunction(word)}`
+  }
+  else if (number === '1'){
+    console.log("algorithm name: ", scoringAlgorithms[1].name);
+    return `scorerFunction result: ${scoringAlgorithms[1].scorerFunction(word)}`;
+  }
+  else if (number === '2'){
+      console.log("algorithm name: ", scoringAlgorithms[2].name);
+      return `scorerFunction result: ${scoringAlgorithms[2].scorerFunction(word)}`
+    }
+  }
+
+
+function transform(object) {};
+
+let newPointStructure = transform(oldPointStructure);
 
 function runProgram() {
-   initialPrompt();
+   let word = initialPrompt();
+   console.log(scorerPrompt(word))
    
 }
 
